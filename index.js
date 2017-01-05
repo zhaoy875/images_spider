40efc0f1a7f4949461f6da9eb663b0f81cfe89ad
zhaoy <1174499602@qq.com>
2017-01-06 07:15:51 +0800

    bug-fix &注释

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  * @Author   zhaoy
  * @DateTime 2017-01-04T15:50:58+0800
  */
-const gulp = require('gulp');
+// const gulp = require('gulp');
 const fs = require('fs');
 const path = require('path');
 const request = require('request');
@@ -11,67 +11,81 @@ const cheerio = require('cheerio');
 const colors = require('colors');
 const packagejson = require('./package.json');    
 console.log("版本"+packagejson.version.cyan )
-// 测试站点 jandan.net/ooxx
+
+// 测试站点例如 jandan.net/ooxx/page-1000
 var urltxt    = 'http://jandan.net/ooxx/page-',
-    startpage = 350,//起始页
-    endpage   = 380,
-    sourcedir = 'images'; //资源保存的根目录名
-    
-var picdir   = sourcedir + '/' +startpage + '_' + endpage + '/';
-var creatdir = './' + picdir;
-//创建 图片保存目录
+    startpage = 1000,//起始页
+    endpage   = 1017,
+    sourcedir = 'images', //资源保存的根目录名
+    picdir   = sourcedir + '/' +startpage + '_' + endpage + '/',
+    creatdir = './' + picdir;
+
+// --------------------------------------
+// 创建 图片保存目录
+// --------------------------------------
 fs.mkdir(creatdir, function(err) {
     if (err) {throw err};
-    let newsourcesrc = '目录' + creatdir +'创建成功';
-    console.log(newsourcesrc.rainbow);
+    //let newsourcesrc = '目录' + creatdir +'创建成功';console.log(newsourcesrc.cyan);
 });
-for (var i = startpage; i < endpage; i++) {
-    requrl = urltxt+i;
 
-request(requrl,function (error,response,body){
-	if (!error && response.statusCode == 200) {
-		//console.log(body);
-		analysisData(body); //采集
-	};
-})
-// cheerio解析
-function analysisData(ourdata) {
-	var $ = cheerio.load(ourdata);
-	var meizi = $('.text img').toArray();
-	console.log( "第" + i + "页共" + meizi.length + "张");
-	for (var i=0;i<meizi.length; i++){
-        // indexOf获取的src是否带有前缀http:字符
-        var meizis_src = meizi[i].attribs.src;
-        if ( meizis_src.indexOf("http") > 0 ) {
-            var imgsrc = meizis_src;
-        } else{
-            var imgsrc = 'http:' + meizis_src;
+// --------------------------------------
+// 爬取指定范围页
+// --------------------------------------
+console.log('---------------开始抓取页面内容---------------'.cyan);
+for (var i = startpage; i < endpage; i++) {
+    var requrl = urltxt+i;
+
+    request(requrl,function (error,response,body){
+        if (!error && response.statusCode == 200) {
+            //console.log(body);
+            analysisData(body); //采集主函数
         };
-		console.log(imgsrc.green);
-		var filename = parseUrlForFileName(imgsrc);  //生成文件名
-        downloadImg(imgsrc,filename,function() {
-            console.log(filename + ' done');
-        });
-	}
-}
-//图片命名
-function parseUrlForFileName(address) {
-    var filename = path.basename(address);
-    return filename;
-}
-//NodeJs path API http://nodejs.org/api/path.html#path_path_basename_p_ext
-// request.head请求,fs模块中的createWriteStream来下载到指定目录
-var downloadImg = function(uri, filename, callback){
-    request.head(uri, function(err, res, body){
-    //console.log('content-type:', res.headers['content-type']);  //返回图片的类型
-    //console.log('content-length:', res.headers['content-length']);  //图片大小
-    if (err) {
-        console.log('err: '+ err);
-        return false;
+    })
+    // 数据解析抓取主函数
+    function analysisData(ourdata) {
+        var $ = cheerio.load(ourdata);
+        var meizi = $('.text .view_img_link').toArray();
+        var thispage = "当前页" + meizi.length + "张";console.log( thispage.rainbow);
+        console.time("创建当前页下载任务计时");
+        for (var i=0;i<meizi.length; i++){
+            // indexOf筛选src是否带有前缀http,此处取的是查看原图<a>的href,若$('.text img')则将herf改为src,其他站点类推
+            var meizis_src = meizi[i].attribs.href;
+            if ( meizis_src.indexOf("http") > 0 ) {
+                var imgsrc = meizis_src;
+            } else{
+                var imgsrc = 'http:' + meizis_src;
+            };
+            console.log(imgsrc.green);
+            var filename = parseUrlForFileName(imgsrc);  //生成文件名
+
+            downloadImg(imgsrc,filename,function() {
+                console.log(filename.cyan + ' done');
+            });
+        }
+        console.timeEnd("创建当前页下载任务计时");
     }
-    console.log('请求: '+ res);
-    request(uri).pipe(fs.createWriteStream( picdir +filename)).on('close', callback);
-    //request得到的流pipe到 picdir文件夹下
-    });
-};
+    //图片命名
+    function parseUrlForFileName(address) {
+        var filename = path.basename(address);
+        return filename;
+    }
+    // --------------------------------------
+    // 下载保存
+    // NodeJs path API http://nodejs.org/api/path.html#path_path_basename_p_ext
+    // request.head==》》fs模块createWriteStream写入到指定目录
+    // --------------------------------------
+    var downloadImg = function(uri, filename, callback){
+        request.head(uri, function(err, res, body){
+        console.log('content-type:', res.headers['content-type']);  //返回图片的类型
+        console.log('content-length:', res.headers['content-length']);  //图片大小
+            if (err) {
+                console.log('err: '+ err);
+                return false;
+            }
+            console.log('请求: '+ res);
+            request(uri).pipe(fs.createWriteStream( picdir +filename)).on('close', function(){
+                console.log(filename.cyan +"保存成功");//request的流数据pipe保存到 picdir文件夹下
+            });
+        });
+    };
 };
